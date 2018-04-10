df1a44f0a46f70142387b7614cbfd08e6b678665
mnowak1996 <35339817+mnowak1996@users.noreply.github.com>
2018-04-11 00:46:16 +0200

    w1

---
+++ Unit-converter/src/MainConverter.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainConverter extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainConverter.class.getResource("UnitConverter.fxml"));
+        Parent layout = fxmlLoader.load();
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Generator");
+        primaryStage.show();
+    }
+}
