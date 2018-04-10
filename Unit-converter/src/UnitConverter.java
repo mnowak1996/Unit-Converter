ac68d65bb435d0b562b38923eba57dbbb4b38a2a
mnowak1996 <35339817+mnowak1996@users.noreply.github.com>
2018-04-11 01:20:11 +0200

    cd

--- Unit-converter/src/UnitConverter.java
+++ Unit-converter/src/UnitConverter.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class UnitConverter {
 
@@ -13,7 +14,7 @@ public class UnitConverter {
     @FXML
     private ComboBox comboBox;
     @FXML
-    private TextArea pole;
+    private TextField pole;
     @FXML
     private Label label;
 
@@ -25,10 +26,26 @@ public class UnitConverter {
     private void initialize() {
         comboBox.setItems(conversions);
     }
+    private double cal=2.54;
+    private double wynik;
+    int kontener;
     @FXML
     private void Convert(){
         String value = String.valueOf(comboBox.getValue());
-        pole.setText(value+"2");
+        switch (value){
+            case "Centymetry na cale":
+                double centymetry =Double.parseDouble(pole.getText());
+                wynik=(centymetry/cal)*100;
+                kontener=(int) wynik*100;
+                wynik=(double) kontener/100;
+                label.setText("centymetrów to "+wynik+" cali");
+                break;
+            case "Cale na centymetry":
+                double cale =Double.parseDouble(pole.getText());
+                double wynik=cale*cal;
+                label.setText("cali to "+wynik+" centymetrów");
+                break;
+        }
     }
 
 }
