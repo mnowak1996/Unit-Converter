cebb9d61150af519716cf9862bf28ad936d0fd32
mnowak1996 <35339817+mnowak1996@users.noreply.github.com>
2018-04-11 11:50:39 +0200

    dwie jednostki

--- Unit-converter/src/UnitConverter.java
+++ Unit-converter/src/UnitConverter.java
@@ -6,9 +6,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.text.DecimalFormat;
+
 public class UnitConverter {
 
-    ObservableList<String> conversions = FXCollections.observableArrayList("Centymetry na cale", "Cale na centymetry");
+    ObservableList<String> conversions = FXCollections.observableArrayList("Centymetry na cale",
+            "Cale na centymetry","Metry na centymetry","Centymetry na metry");
     // lista dni tygodnia typu Observable list
 
     @FXML
@@ -28,24 +31,30 @@ public class UnitConverter {
     }
     private double cal=2.54;
     private double wynik;
-    int kontener;
     @FXML
     private void Convert(){
         String value = String.valueOf(comboBox.getValue());
+//        value = value.replaceAll(",",".");
+
         switch (value){
             case "Centymetry na cale":
-                double centymetry =Double.parseDouble(pole.getText());
-                wynik=(centymetry/cal)*100;
-                kontener=(int) wynik*100;
-                wynik=(double) kontener/100;
-                label.setText("centymetrów to "+wynik+" cali");
+                double centymetry =Double.parseDouble(ReplaceComma(pole.getText()));
+                wynik=(centymetry/cal);
+                label.setText("centymetrów to "+new DecimalFormat("##.##").format(wynik)+" cali");
                 break;
             case "Cale na centymetry":
-                double cale =Double.parseDouble(pole.getText());
+                double cale =Double.parseDouble(ReplaceComma(pole.getText()));
                 double wynik=cale*cal;
                 label.setText("cali to "+wynik+" centymetrów");
                 break;
         }
+    }
+
+    private String ReplaceComma (String a){
+        if(a.contains(",")) {
+            a = a.replaceAll(",", ".");
+        }
+        return a;
     }
 
 }
